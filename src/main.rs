f35bf8af892d469b7b4d5bfffbc9c4a784ebfacc
CristoJV <44086220+CristoJV@users.noreply.github.com>
2025-11-22 23:21:04 +0100

    feat(pio): Blinking LED with PIO

--- src/main.rs
+++ src/main.rs
@@ -2,65 +2,65 @@
 #![no_main]     // Don't use the normal main entry point provided by std
 
 use rp_pico::entry; // Entry point macro for bare-metal programs
-use rp_pico::hal;   // Import the HAL (Hardware Abstraction Layer)
-use rp_pico::hal::clocks::Clock;
-use embedded_hal::digital::OutputPin; // Trait for controlling digital output pins
-use cortex_m::Peripherals as CorePeripherals;
+use rp_pico::hal as hal;   // Import the HAL (Hardware Abstraction Layer)
+use hal::gpio::{FunctionPio0, Pin};
+use hal::pac;
+use hal::Sio;
+use hal::pio::PIOExt;
+
+// Import PIO crates
+use hal::pio::PIOBuilder;
+use pio::{Assembler, SetDestination};
+
+use cortex_m;
 use panic_halt as _; // Panic handler that simply halts the CPU
 
 #[entry] // Marks the function that will run at startup
 fn main() -> ! {
 
     // Peripherals::take() gives access to all hardware blocks (GPIO, clocks)
-    let mut pac = rp_pico::pac::Peripherals::take().unwrap();
-
-    let core = CorePeripherals::take().unwrap();
-
-    // Initialize system clocks
-    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
-
-    // Initialize PLLs and clock tree using Pico's 12 MHz crystal oscillator
-    let clocks = hal::clocks::init_clocks_and_plls(
-        rp_pico::XOSC_CRYSTAL_FREQ, // 12 MHz external crystal
-        pac.XOSC,                   // Crystal oscillator block
-        pac.CLOCKS,                 // Clock control block
-        pac.PLL_SYS,                // System PLL
-        pac.PLL_USB,                // USB PLL
-        &mut pac.RESETS,            // Reset controller
-        &mut watchdog               // Watchdog
-    )
-    .ok()
-    .unwrap(); // If any part fails, panic
-
-    // Configure GPIO pins
-    // SIO (Single-Cycle IO) gives fast access to each pin
-    let sio = hal::Sio::new(pac.SIO);
-
-    let pins = rp_pico::Pins::new(
+    let mut pac = pac::Peripherals::take().unwrap();
+    let sio = Sio::new(pac.SIO);
+    let pins = hal::gpio::Pins::new(
         pac.IO_BANK0,              // Actual GPIO hardware block
         pac.PADS_BANK0,            // GPIO pad configuration (pull ups,...)
-        sio.gpio_bank0,           // SIO control of GPIO
+        sio.gpio_bank0,            // SIO control of GPIO
         &mut pac.RESETS            // Reset controller
     );
 
-    // Configure LED pin as output
-    // On Raspberry Pi Pico, the onboard LED is connected to GPIO25.
-    let mut led_pin = pins.led.into_push_pull_output();
+    // Configure LED pin for Pio0
+    let led: Pin <_, FunctionPio0, _> = pins.gpio25.into_function();
+    // Get PIN id for use inside PIO
+    let led_pin_id = led.id().num;
 
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
-    // Main loop
+    const MAX_DELAY: u8 = 31;
+    let mut ass = Assembler::<32>::new();
+    let mut wrap_target = ass.label();
+    let mut wrap_source = ass.label();
+    // Set pin as output
+    ass.set(SetDestination::PINDIRS, 1);
+    // Define begin of program loop
+    ass.bind(&mut wrap_target);
+    // Set pin low
+    ass.set_with_delay(SetDestination::PINS,0,MAX_DELAY);
+    // Set pin high
+    ass.set_with_delay(SetDestination::PINS,1,MAX_DELAY);
+    // Define end of program loop
+    ass.bind(&mut wrap_source);
+
+    let program = ass.assemble_with_wrap(wrap_source, wrap_target);
+
+    // Initialize and start PIO
+    let (mut pio, sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
+    let installed = pio.install(&program).unwrap();
+    let (int, frac) = (0, 0);
+    let (sm, _, _) = PIOBuilder::from_installed_program(installed)
+        .set_pins(led_pin_id, 1)
+        .clock_divisor_fixed_point(int, frac)
+        .build(sm0);
+    sm.start();
+ 
     loop {
-        // Turn LED ON
-        led_pin.set_high().unwrap();
-
-        // Busy-wait delay. The RR2040 executes ~1 instruction per cycle
-        // at default clock speeds
-        delay.delay_ms(1000);
-
-        // Turn LED OFF
-        led_pin.set_low().unwrap();
-
-        // Delay again
-        delay.delay_ms(500);
+        cortex_m::asm::wfi();
     }
 }
